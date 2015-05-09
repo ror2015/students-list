5ca709bf7b516cef956306cbc994b330165b8eb9
Sakir Temel <sakirtemel@gmail.com>
2015-05-09 14:53:35 +0200

    Add full calendar without gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require selectize
+//= require moment.min
 //= require_tree .
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
   $('#student_name').selectize({
     sortField: 'text'
   });
+
+  $('#calendar').fullCalendar({
+    // put your options and callbacks here
+    lang: 'pl'
+  });
 });
